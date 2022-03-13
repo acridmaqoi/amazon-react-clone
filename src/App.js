c6a56191cf2bc7ec614a86cf0f956bbad10517f4
Sam Quinn <samquinn20@gmail.com>
2022-03-13 19:41:13 +0000

    add login page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import "./App.css";
 import Checkout from "./Checkout";
 import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
---
+++ src/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    // firebase auth user
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    // firebase auth user
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="login__signInButton">
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the Amazon conditions of use & sale. Please
+          see our Privacy Notice, our Cookies Notice and our Intrest-Based Ads
+          Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
